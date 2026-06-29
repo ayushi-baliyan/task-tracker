b8f5cff28dbda473125494fc9f9fd853194f9f36
Ayushi1721 <ayushi.baliyan_cs22@gla.ac.in>
2026-06-29 11:21:00 +0530

    Update production API URL

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-tracker-backend-4tjp.onrender.com",
 });
 
 export default API;
